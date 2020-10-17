53192171fbe7c5a05bf8b9cabe7f47eecc0f2d2a
CJ Dodge <hitmancjack@yahoo.com>
2020-10-16 19:13:53 -0700

    added classes to js files and added jest to package.json

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//External Packages
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,6 +6,7 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+//Internal Modules
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -13,6 +15,8 @@ const render = require("./lib/htmlRenderer");
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
